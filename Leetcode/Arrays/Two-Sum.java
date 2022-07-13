9d8586443abc9101f1d88109ddbe11d3c695602b
Ravi Singh Bajetha <84458346+ravi-singh-100@users.noreply.github.com>
2022-07-13 21:13:14 +0530

    Create Two-Sum.java

---
+++ Leetcode/Arrays/Two-Sum.java
@@ -0,0 +1,30 @@
+
+// Ques: https://leetcode.com/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+     int []output=new int[2];
+       
+			 int start=0;
+			 
+			 int end=nums.length-1;
+			 while(start<=end) {
+				 if(nums[start]+nums[end]<target) {
+					 ++start;
+				 }
+				 else if(nums[start]+nums[end]>target) {
+			 --end;
+				 }
+				 else {
+                     output[0]=nums[start];
+                     output[1]=nums[end];
+					 
+                      ++start;
+						 --end;
+				 }
+					
+			 }
+        return output;
+    }
+}
