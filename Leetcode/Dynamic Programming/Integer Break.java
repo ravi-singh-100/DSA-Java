5911b3156c2408aaa239405d45b32505368a946d
Ravi Singh Bajetha <84458346+ravi-singh-100@users.noreply.github.com>
2023-08-09 21:16:07 +0530

    Create Integer Break.java

---
+++ Leetcode/Dynamic Programming/Integer Break.java
@@ -0,0 +1,13 @@
+// Ques:https://leetcode.com/problems/integer-break/
+
+class Solution {
+    public int integerBreak(int n) {
+       if(n==2||n==3) return n-1;
+       int result=1;
+       while(n>4){
+           n-=3;
+           result*=3;
+       } 
+       return (n*result);
+    }
+}
